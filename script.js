7f3296be4a11618b31b5f96afd9d140c92557660
Nao2104 <naomyromero@gmail.com>
2022-10-17 16:36:11 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,95 @@
+
+//SEGUNDO FORMULARIO
+
+function Print() {
+    let users = {
+    iname: document.getElementById("iname").value,
+    Lastnames: document.getElementById("Lastnames").value,
+    Idnumber: document.getElementById("Idnumber").value,
+    Age: document.getElementById("Age").value,
+    Position: document.getElementById("Position").value,
+    Rh: document.getElementById("Rh").value,
+    Telephone: document.getElementById("Telephone").value,
+    }
+        
+  console.log (users);
+  
+  return
+  
+  }
+  
+  
+  //FORMULARIO CARNETIZACIÓN//
+
+function sayMeYourName() {
+    let name = prompt("Type your name");
+  
+    console.log(name);
+    return;
+       }
+      
+function sayMeYourLastname() {
+    let Lastname = prompt("Type your Lastname");
+      
+    console.log(Lastname);
+    return;
+           }
+
+
+
+function sayMeYourAge() {
+    let Age = prompt("Type your Age");
+  
+    console.log(Age);
+    return;
+       }
+
+      
+  
+function sayMeYourPosition() {
+        let Positioninthecompany = prompt("Type your Positioninthecompany");
+      
+        console.log(Positioninthecompany);
+        return;
+           }
+
+   
+
+    function sayMeYourRh() {
+        let Rh = prompt("Type your Rh");
+      
+        console.log(Rh);
+        return;
+           }
+
+   
+    function sayMeYourPosition() {
+        let Positioninthecompany = prompt("Type your Positioninthecompany");
+      
+        console.log(Positioninthecompany);
+        return;
+           }
+
+    
+    function sayMeYourIdnumber() {
+        let Idnumber = prompt("Type your Idnumber");
+      
+        console.log(Idnumber);
+        return;
+           }
+
+    function sayMeYourTelephone() {
+        let Telephonenumber = prompt("Type your Telephonenumber");
+      
+        console.log(Telephonenumber);
+        return;
+           }
+
+    function Print(){
+        let Print = alert("Do you want to print?");
+        return;
+    }
+
+
+  
+  
